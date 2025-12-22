888bd52ac9974af00f2704b2f213dd8ea950379a
Oliver Zihler <oliver.zihler@codeartify.com>
2025-12-22 10:37:12 +0100

    refactor drop down

--- frontend/fitnesslab/src/app/auth/user-profile-drop-down-menu.ts
+++ frontend/fitnesslab/src/app/auth/user-profile-drop-down-menu.ts
@@ -3,47 +3,36 @@ import {TranslatePipe} from '@ngx-translate/core';
 import {UserProfile} from './auth.service';
 import {UserProfileTitle} from './user-profile-title';
 import {UserProfileEmail} from './user-profile-email';
+import {UserProfileRoles} from './user-profile-roles';
 
 @Component({
   selector: 'gym-user-profile-drop-down-menu',
   imports: [
     TranslatePipe,
     UserProfileTitle,
-    UserProfileEmail
+    UserProfileEmail,
+    UserProfileRoles
   ],
   template: `
     @let profile = userProfile();
 
     @if (isOpen()) {
-      <div
-        class="absolute right-0 mt-2 w-64 bg-white rounded-lg shadow-xl border border-gray-200 py-2 z-50"
-      >
+      <div class="absolute right-0 mt-2 w-64 bg-white rounded-lg shadow-xl border border-gray-200 py-2 z-50">
         <div class="px-4 py-3 border-b border-gray-200">
           <gym-user-profile-title [userProfile]="profile"/>
           <gym-user-profile-email [email]="profile.email"/>
-
-          <!-- rolese -->
-          <div class="flex gap-1 mt-2">
-            @for (role of profile.roles; track role) {
-              <span
-                class="inline-block px-2 py-1 text-xs font-medium bg-blue-100 text-blue-800 rounded"
-              >
-                    {{ role }}
-                  </span>
-            }
-          </div>
+          <gym-user-profile-roles [roles]="profile.roles"/>
         </div>
 
-        <button
-          (click)="logout()"
-          class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 transition-colors"
-        >
+        <button class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 transition-colors"
+                (click)="logout()">
           {{ 'auth.logout' | translate }}
         </button>
       </div>
     }
-    <!-- Backdrop -->
+
     @if (isOpen()) {
+      <!-- Backdrop -->
       <div
         (click)="closeMenu()"
         (keydown.enter)="closeMenu()"
--- frontend/fitnesslab/src/app/auth/user-profile-roles.ts
+++ frontend/fitnesslab/src/app/auth/user-profile-roles.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 
 @Component({
   selector: 'gym-user-profile-roles',
-  imports: [],
   template: `
-    <p>
-      user-profile-roles works!
-    </p>
+    <div class="flex flex-col gap-1 mt-2">
+      @for (role of roles(); track role) {
+        <span
+          class="block w-full px-2 py-1 text-xs font-medium bg-blue-100 text-blue-800 rounded"
+        >
+          {{ role }}
+        </span>
+      }
+    </div>
   `,
-  styles: ``,
 })
 export class UserProfileRoles {
-
+  roles = input.required<string[]>();
 }
